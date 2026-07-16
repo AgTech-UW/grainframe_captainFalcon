083e94c55b6818d2437ba60de93ee36bee7c8ce8
GDII <george.m.dyck@gmail.com>
2026-07-15 19:13:04 -0500

    editing some error

--- run_all.py
+++ run_all.py
@@ -1,5 +1,4 @@
 """Run everything, equivalent to the old monolithic captainFalcon.py
-(plus the new Experiment 4: Falcon end-pose error).
 
 Usage:  python run_all.py [dataTag]
 """
@@ -8,7 +7,7 @@ from matplotlib import pyplot as plt
 from grainframe import SimOptions, load_dataset
 
 sys.path.insert(0, 'experiments')
-from experiments import exp1_ab_distractors, exp2_fanboids, exp3_sweep_chaos, exp4_falcon_endpose
+from experiments import exp1_ab_distractors, exp2_fanboids, exp3_sweep_chaos
 import numpy as np
 
 dataTag = sys.argv[1] if len(sys.argv) > 1 else '1'
@@ -20,6 +19,5 @@ print()
 baselineXY = np.column_stack([runA['x'], runA['y']])
 exp2_fanboids.run(opts, data, baselineXY=baselineXY)
 exp3_sweep_chaos.run(opts, data)
-exp4_falcon_endpose.run(opts, data)
 
 plt.show()
